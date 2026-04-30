b7a202a5f4d929ae20084aea805e32f8aaf62fca
abisara2k1 <abisara@iu.edu>
2026-04-29 23:57:16 -0400

    Bug fix

--- src/main/java/com/rpl/config/WebConfig.java
+++ src/main/java/com/rpl/config/WebConfig.java
@@ -9,7 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5500", "http://127.0.0.1:5500")
+                .allowedOrigins(
+                        "http://localhost:5500",
+                        "http://127.0.0.1:5500",
+                        "https://abisara-p532-spring2026.github.io"
+                )
+                .allowedOriginPatterns("https://*.github.io")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
